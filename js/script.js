be096bd9169dc5a5df9216056a2bcc2b6b7f094f
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-07-07 23:54:49 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var fName = "Foysal";
+var lName = "hasib";
+
+console.log(fName);
+document.write(lName);
+//=========================================
+//=========================================
+let father = "Golam wahab";
+let mother = "Jhorna Begum";
+
+console.log(father);
+console.log(mother);
+
+//==========================================
+//==========================================
+
+const brother = "Bappy";
+const sister = "Tania";
+
+console.log(brother);
+console.log(sister);
